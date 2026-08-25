c34cf4319d800e6bb6efd26259a5083cad0dc312
YouG-o <xoyougoox@gmail.com>
2026-08-25 10:24:47 +0200

    feat: add OutputOption enum and ProcessingProgress DTO for workflow management

---
+++ src/main/java/com/substack/application/dto/OutputOption.java
@@ -0,0 +1,16 @@
+package com.substack.application.dto;
+
+/**
+ * Output target options for generated bilingual subtitles.
+ */
+public enum OutputOption {
+    /**
+     * Generate an external .ass file next to the input video.
+     */
+    EXTERNAL_ASS,
+
+    /**
+     * Embed the generated ASS track directly into the MKV container (remuxing).
+     */
+    EMBED_IN_MKV
+}
